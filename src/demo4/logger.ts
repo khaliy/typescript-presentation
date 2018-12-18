62632c97146133073b60b85398f7dc0085d1e52a
Tamas Szebeni <Tamas_Szebeni@epam.com>
2018-12-18 05:04:36 -0700

    Update logger.ts

--- src/demo4/logger.ts
+++ src/demo4/logger.ts
@@ -1,7 +1,7 @@
 export const log: MethodDecorator = (_: object, __: string | symbol, descriptor: PropertyDescriptor) => {
     const original: () => any = descriptor.value;
-    descriptor.value = function(...args: any) {
+    descriptor.value = function(...args: any[]) {
         console.log(`call method ${original.name} with args ${JSON.stringify(args)}`);
-        return original.apply(this, args);
+        return original.apply(this, args as []);
     };
 };
