ce12b7584315d5c0d0c220160fb3adf695348973
Tamas Szebeni <Tamas_Szebeni@epam.com>
2018-12-18 04:46:20 -0700

    add tsc build and artifacts

--- src/demo4/logger.ts
+++ src/demo4/logger.ts
@@ -1,6 +1,6 @@
 export const log: MethodDecorator = (_: object, __: string | symbol, descriptor: PropertyDescriptor) => {
     const original: () => any = descriptor.value;
-    descriptor.value = function(...args: any[]) {
+    descriptor.value = function(...args: any) {
         console.log(`call method ${original.name} with args ${JSON.stringify(args)}`);
         return original.apply(this, args);
     };
